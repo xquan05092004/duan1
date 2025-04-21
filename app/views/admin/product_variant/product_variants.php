4c981998d46a643b3dda51c864b0942e3e9ba3a2
xquan05092004 <xquan05092004@gmail.com>
2025-04-21 13:22:15 +0700

    a

--- app/views/admin/product_variant/product_variants.php
+++ app/views/admin/product_variant/product_variants.php
@@ -97,7 +97,7 @@ $variants = $db->runQuery($query);
 
         <!-- Sidebar -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <a href="../../index3.html" class="brand-link">
+            <a href="index.php" class="brand-link">
                 <img src="assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">AdminLTE 3</span>
